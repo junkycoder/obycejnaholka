211bdac6bad9b36a5be53acb0ed8107fddefa049
Dan Hromada <hromada.dan@gmail.com>
2016-08-21 17:36:10 +0200

    Fix.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('*', (req, res) => {
   res.status(200).send('Veronika Hallerová - obyčejná holka');
 });
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Running on port', port);
