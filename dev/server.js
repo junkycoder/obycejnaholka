d55b904c951f3f5f91e819560f4ec7b5b10ad370
Dan Hromada <hromada.dan@gmail.com>
2016-08-23 19:42:25 +0200

    Simplify.

--- dev/server.js
+++ dev/server.js
@@ -4,18 +4,11 @@ import config from '../webpack.config';
 
 export function start (port) {
   const server = new WebpackDevServer(webpack(config), {
-    //publicPath: config.output.publicPath,
-    //hot: true,
     noInfo: true
   });
 
 
-  // Start server
-  server.listen(port, 'localhost', (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`Devserver running on http://localhost:${port}/`);
-    }
+  server.listen(port, (err, result) => {
+    if (err) throw (err);
   });
 }
